27c2ddf4814c9008b214f07e98fc50cba87567e6
Silvia <silvialux98@gmail.com>
2021-04-05 12:02:42 -0500

    Modificamos el APP

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
-var url = window.location.href;
-var swLocation = '/ChatPecados/sw.js';
+var url= window.location.href;
+var swLocation= '/ChatPecados/sw.js';
 
-if(navigator.serviceWorker){
+if ( navigator.serviceWorker ) {
     if(url.includes('localhost')){
-        swLocation = '/sw.js';
+        swLocation='/sw.js';
     }
+    navigator.serviceWorker.register(swLocation);
 }
 
 // Referencias de jQuery
